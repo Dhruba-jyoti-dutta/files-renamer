b3685df048274f8b27901aa3110e2b82dd4ed346
Dhruba-jyoti-dutta <sddhruba.857@gmail.com>
2023-03-20 02:24:13 +0530

    modified:   renamer.py

--- renamer.py
+++ renamer.py
@@ -1,21 +1,13 @@
 import os
-folder="F:/python/not so imp/renamer/renamer data/"
+folder="D:/sharex/Screenshots/2023-03/"
 os.chdir(folder)
-# files=[]
-# for file in sorted(os.listdir(folder), key=os.path.getctime):
-#     files.append(file)
-# print(files)
-# above line will be same as os.chdir("F:/python/not so imp/renamer/renamer data/")
-# i=0
-# for file in files:
-#     # file.sort(key=os.path.getctime)
-#     # print(file)
-#     os.rename(file,f"{i}.png")
-#     i=i+1
+
 
 
 def rename():
     os.chdir(folder)
+
+    #files gita sort kore
     files=[]
     for file in sorted(os.listdir(folder), key=os.path.getctime):
         files.append(file)
